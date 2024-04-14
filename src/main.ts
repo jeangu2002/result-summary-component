dc42eb9b57939ec69dd1c5dd6e5c07813d13fbc3
Jean Gustave NSABIYERA <jeangustave.nsabiyera@arhs-cube.com>
2024-04-14 22:52:47 +0200

    js animations

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,31 @@ function loadData(): Stat[] {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  const stats = loadData();
-  console.log(stats);
+  const stats = Array.from(document.querySelectorAll('.stat'));
+  const score = document.querySelector('.score > strong');
+  const scoreParagraph:HTMLElement | null = document.querySelector('.score > p')
+  let timeout = 250;
+  stats.forEach(stat => {
+    setTimeout(() => {
+      stat.classList.add('active');
+    }, timeout);
+    timeout += 250;
+  });
+
+  setTimeout(() => {
+      let count = 0;
+      scoreParagraph!.style.display = 'block';
+      const interval = setInterval(()=>{
+          score!.textContent = `${count}`;
+          count++;
+          if(count === 73) {
+            clearInterval(interval);
+          }
+      },50)
+  }, 1250);
+
+  setTimeout(() => {
+      document.querySelector('.appreciation')?.classList.add('visible');
+  }, 5000)
 
 })
